6b93139cff69008ffcb12fc41c89ebae8da378b8
LiorAharon98 <lior.ah98@gmail.com>
2023-01-26 16:31:38 +0200

    update4

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -10,18 +10,21 @@ const Projects = () => {
       info: "An End to end progressive web application for online shopping served with a full functional backend including authentication and Database manipulation",
       link: "https://lioraharon98.github.io/bank/",
       img: "/bank.jpg",
+      using: ["react", "Html5", "Css3", "NodeJs", "ExpressJs", "MongoDb", "Firebase"],
     },
     {
       label: "super market",
       info: "An End to end progressive web application for online shopping served with a full functional backend including authentication and Database manipulation",
       link: "https://lioraharon98.github.io/supermarket/",
       img: "/supermarket.webp",
+      using: ["react", "Html5", "Css3", "NodeJs", "ExpressJs", "MongoDb", "Firebase", "ejs"],
     },
     {
       label: "decibel meter",
       info: "an decibel meter that can detect a exception in db by the day",
       link: "https://next-js-decibel-meter.herokuapp.com/",
       img: "/decibel-meter.png",
+      using: ["react", "Html5", "Css3", "NodeJs", "ExpressJs", "MongoDb", "Firebase"],
     },
   ];
   const toggleProjects = (value) => {
@@ -31,19 +34,26 @@ const Projects = () => {
     setIndex(index);
   };
   return (
-    <div className={styles.page_container}>
+    <div key={index} className={styles.container}>
+      <img className={styles.img} src={projects[index].img} alt="error"></img>
       <div className={styles.arrow_container}>
         <AiOutlineArrowLeft className={styles.arrow} onClick={toggleProjects} />
-
+        <a className={styles.link} href={projects[index].link}>
+      link to website
+      </a>
         <AiOutlineArrowRight className={styles.arrow} onClick={toggleProjects.bind(this, "plus")} />
       </div>
-      <div key={index} className={styles.container}>
-        <img className={styles.img} src={projects[index].img} alt="error"></img>
-        <h2 className={styles.info}>{projects[index].info}</h2>
-        <a className={styles.link} href={projects[index].link}>
-          Click
-        </a>
+      <div className={styles.info_container}>
+        <h2>project name : {projects[index].label}</h2>
+        <h2 className={styles.info}> project info : {projects[index].info}</h2>
+        <div className={styles.using_test}>
+          <h2 className={styles.info}>using : </h2>
+          {projects[index].using.map((using, index) => (
+            <h2 key={index}>{using},</h2>
+          ))}
+        </div>
       </div>
+    
     </div>
   );
 };

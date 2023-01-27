ee0efd30c4b3363d4bd6012d47fee8a838c90161
LiorAharon98 <lior.ah98@gmail.com>
2023-01-27 21:30:23 +0200

    update6

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -2,6 +2,7 @@ import styles from "./homepage.module.css";
 import { AiFillLinkedin } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
 import { SiGmail } from "react-icons/si";
+import { AiOutlineFilePdf } from "react-icons/ai";
 import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
@@ -10,7 +11,10 @@ const Homepage = () => {
         <div className={styles.about_container}>
           <h1>HI There , I'm Lior Aharon</h1>
           <p className={styles.text}>i am a full stack developer</p>
-          <Link to={'/about'} className={styles.about_button}> About me</Link>
+          <Link to={"/about"} className={styles.about_button}>
+            {" "}
+            About me
+          </Link>
           <div className={styles.button_container}>
             <a href="https://www.linkedin.com/in/lior-aharon-714760245" className={styles.button}>
               <AiFillLinkedin />
@@ -20,6 +24,13 @@ const Homepage = () => {
             </a>
             <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=lior.ah98@gmail.com" className={styles.button}>
               <SiGmail />
+            </a>
+          </div>
+          <div className={styles.resume_container}>
+            {/* <AiOutlineFilePdf fontSize={'20px'}/> */}
+            <a className={styles.resume} href="/Resume Lior Aharon.pdf">
+              {" "}
+              my resume
             </a>
           </div>
         </div>
